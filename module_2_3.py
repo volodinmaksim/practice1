cf78ad24f0317b8d6a3dc90eca9c5173f7705125
volodin543 <143259438+volodinmaksim@users.noreply.github.com>
2024-09-08 17:10:31 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,8 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+count = 0
+while count < len(my_list)-1:
+    if my_list[count] > 0:
+        print(my_list[count])
+    if my_list[count] < 0:
+        break
+    count += 1
