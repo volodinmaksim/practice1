11ab78b2e9e362d46c2b4d237c9c8a4dd43c7d29
volodin543 <143259438+volodinmaksim@users.noreply.github.com>
2024-09-09 11:04:09 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,6 +1,6 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 count = 0
-while count < len(my_list)-1:
+while count < len(my_list):
     if my_list[count] > 0:
         print(my_list[count])
     if my_list[count] < 0:
