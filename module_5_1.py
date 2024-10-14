48fbf1cc630657c94ae894ef8ab79cc7454fab1a
volodin543 <143259438+volodinmaksim@users.noreply.github.com>
2024-10-14 22:59:07 +0300

    Add files via upload

---
+++ module_5_1.py
@@ -0,0 +1,18 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print("Такого этажа не существует")
+            return
+        count = 1
+        while count <= new_floor:
+            print(count)
+            count += 1
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
