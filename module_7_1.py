40f69a78cd8eb49d4e9f2f5d67a860f8b963f76e
volodin543 <143259438+volodinmaksim@users.noreply.github.com>
2024-12-12 01:45:40 +0300

    Add files via upload

---
+++ module_7_1.py
@@ -0,0 +1,40 @@
+class Product:
+    def __init__(self, name, weight, category):
+        self.name = name
+        self.weight = weight
+        self.category = category
+
+    def __str__(self):
+        return f"{self.name}, {self.weight}, {self.category}"
+
+class Shop:
+    def __init__(self):
+        self.__file_name = 'products.txt'
+
+    def get_products(self):
+        file = open(self.__file_name, "r")
+        st = file.read()
+        file.close()
+        return st
+
+    def add(self, *products):
+        file_reed = self.get_products()
+        file = open(self.__file_name, "a")
+        for product in products:
+            if product.name in file_reed:
+                print(f"Продукт {product.name} уже есть в магазине")
+            else:
+                file.write(str(product) + '\n')
+        file.close()
+
+
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2) # __str__
+
+s1.add(p1, p2, p3)
+
+print(s1.get_products())
