28a68b4d9d000fc06699c3dddd94feda250e381f
volodin543 <143259438+volodinmaksim@users.noreply.github.com>
2024-12-12 23:24:04 +0300

    Add files via upload

---
+++ format_string.py
@@ -0,0 +1,22 @@
+team1_num = 5
+team2_num = 6
+print("В команде Мастера кода участников: %s" % (team1_num))
+print("Итого сегодня в командах участников: %s и %s !" %(team1_num, team2_num) )
+
+
+score_2 = 42
+print("Команда Волшебники данных решила задач: {} !".format(score_2))
+
+team1_time = 18015.2
+print("Волшебники данных решили задачи за {} с !".format(team1_time))
+
+
+score_1 = 40
+print(f"Команды решили {score_1} и {score_2} задач.")
+
+challenge_result = "победа команды Мастера кода"
+print(f"Результат битвы: {challenge_result} !")
+
+tasks_total = 82
+time_avg = 350.4
+print(f"Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу!.")
